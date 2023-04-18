59ee33000f99b8abdf49670ae41b4740e539b846
antoKeinanen <anto.keinanen@gmail.com>
2023-04-18 15:51:22 +0300

    add better truth table output

--- src/truth_table_solver.rs
+++ src/truth_table_solver.rs
@@ -1,6 +1,19 @@
+use std::{iter::zip, collections::HashMap};
+
 use dialoguer::Input;
-use logical_solver::{permutate, solve_truth_table, parse_expression};
+use logical_solver::{permutate, solve_truth_table, parse_expression, ast::Expr};
 use spinner::SpinnerBuilder;
+
+fn print_table(vars: Vec<String>, states: Vec<HashMap<String, bool>>, results: Vec<bool>, expr: Expr) {
+    
+    vars.iter().for_each(|var| print!("{}\t|", var));
+    print!("{:?}\n", expr);
+
+    for (state, result) in zip(states, results) {
+        state.values().for_each(|state| print!("{}\t|", state));
+        print!("{}\n", result);
+    }
+}
 
 pub fn solve_truth_table_loop() {
     loop {
@@ -9,7 +22,7 @@ pub fn solve_truth_table_loop() {
             .interact_text()
             .unwrap();
 
-        let vars = vars.split(", ").map(|var| var.to_string()).collect();
+        let vars:Vec<String> = vars.split(", ").map(|var| var.to_string()).collect();
 
         let expr: String = Input::new()
             .with_prompt("Enter expression")
@@ -18,7 +31,7 @@ pub fn solve_truth_table_loop() {
 
 
         let sp = SpinnerBuilder::new("Generating permutations...".into()).start();
-        let states = permutate(vars);
+        let states = permutate(vars.clone());
 
         sp.message("Parsing expression...".into());
         let expr = parse_expression(expr.as_str());
@@ -26,10 +39,10 @@ pub fn solve_truth_table_loop() {
         match expr {
             Ok(expr) => {
                 sp.message("Evaluating all permutations...".into());
-                let results = solve_truth_table(expr, states);
+                let results = solve_truth_table(expr.clone(), states.clone());
                 sp.close();
-
-                println!("-> {:?}", results);
+                
+                print_table(vars, states, results, *expr);
             },
             Err(err) => {
                 sp.close();
